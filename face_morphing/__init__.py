9229f887d1b432604b7768e0f3c20bff6ff84c64
pierow2k <pierow2k@foo.org>
2026-05-17 15:10:52 -0500

    refactor(model): move model file to model directory

--- face_morphing/__init__.py
+++ face_morphing/__init__.py
@@ -7,7 +7,7 @@ from pathlib import Path
 
 import dlib
 
-from ._typing import ImageArray
+from ._typing import Any, ImageArray
 from .delaunay_triangulation import compute_delaunay_triangles
 from .face_landmark_detection import align_faces
 from .face_morph import generate_morph_sequence
@@ -15,14 +15,18 @@ from .face_morph import generate_morph_sequence
 logger = logging.getLogger(__name__)
 
 
+class NoFaceFoundError(Exception):
+    """Raised when there is no face found."""
+
+
 # Define the path relative to this file
 _MODEL_PATH = (
-    Path(__file__).parent / "utils" / "shape_predictor_68_face_landmarks.dat"
+    Path(__file__).parent / "models" / "shape_predictor_68_face_landmarks.dat"
 )
 
 
 @lru_cache(maxsize=1)
-def _get_detector() -> dlib.frontal_face_detector:
+def _get_detector() -> Any:
     """Lazy-loads the dlib face detector."""
     return dlib.get_frontal_face_detector()
 
